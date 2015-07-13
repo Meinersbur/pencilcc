34c94d4a6e12a040b3695f011ff07d7b194eb6f5
Michael Kruse <pencilcc@meinersbur.de>
2015-07-13 16:58:07 +0200

    Fix distcheck

--- get-scala.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-set -e # Fail if any subcommand fails
-
-SCALA_HOME=./scala
-mkdir -p $SCALA_HOME
-wget http://www.scala-lang.org/files/archive/scala-2.10.4.tgz -O /tmp/scala-2.10.4.tgz 
-tar xf /tmp/scala-2.10.4.tgz --strip-component=1 -C $SCALA_HOME
-rm /tmp/scala-2.10.4.tgz
