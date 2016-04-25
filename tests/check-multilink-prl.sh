864bf17a6f35f632cd3b9aff66d63682cfe8d644
Michael Kruse <pencilcc@meinersbur.de>
2016-04-25 17:46:32 +0200

    Refinement to accept C++ files

--- tests/check-multilink-prl.sh
+++ tests/check-multilink-prl.sh
@@ -10,8 +10,8 @@ cd "${TESTBUILDDIR}"
 
 echo "Compiling..."
 ${PENCILCC} -c -vv -O3 --target=prl -Werror "${TESTSRCDIR}/multilink_main.c" -o multilink_main.o
-${PENCILCC} -c -vv -O3 --target=prl -Werror "${TESTSRCDIR}/multilink_kernel1.pencil.c" -o multilink_kernel1.o
-${PENCILCC} -c -vv -O3 --target=prl -Werror "${TESTSRCDIR}/multilink_kernel2.pencil.c" -o multilink_kernel2.o
+${PENCILCC} -c -vv -O3 --target=prl -Werror "${TESTSRCDIR}/multilink_kernel1.pencil.c" -o multilink_kernel1.o --keep
+${PENCILCC} -c -vv -O3 --target=prl -Werror "${TESTSRCDIR}/multilink_kernel2.pencil.c" -o multilink_kernel2.o --keep
 
 echo "Linking..."
 ${PENCILCC} -vv -O3 --target=prl -Werror --autorpath multilink_main.o multilink_kernel1.o multilink_kernel2.o -o multilink-prl
