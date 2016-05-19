c6c15cdf9cc88b39d7dadc0d91069b394c97b0cd
Michael Kruse <pencilcc@meinersbur.de>
2016-05-19 16:41:56 +0200

    Add and test additional memory functions

--- tests/prl_manage_host.c
+++ tests/prl_manage_host.c
@@ -1,12 +1,6 @@
-
-
-
 #include <stdlib.h>
+#include <assert.h>
 #include <prl_mem.h>
-
-
-
-
 
 static void kernel(int n, float A[static const restrict n], float val) {
 #pragma scop
@@ -16,20 +10,25 @@ static void kernel(int n, float A[static const restrict n], float val) {
 #pragma endscop
 }
 
-
 int main() {
 	float A[128];
 	prl_mem mem_A = prl_mem_manage_host(sizeof A, A, prl_mem_readable_writable);
+	assert(mem_A);
 
 	kernel(128, A, 42);
+
+	prl_mem mem_A2 = prl_mem_manage_host(sizeof A, A, prl_mem_readable_writable);
+	assert(mem_A == mem_A2);
+	assert(mem_A == prl_get_mem(A));
+	assert(prl_mem_get_host_mem(mem_A)== &A);
+
 	kernel(128, A, 21);
 
 	prl_mem_free(mem_A);
 
-	for (int i = 0; i<128; i+=1) {
-		if (A[i] != 21)
-			exit(1);
-	}
+
+	for (int i = 0; i<128; i+=1)
+		assert(A[i] == 21);
 	
 	return EXIT_SUCCESS;
 }
